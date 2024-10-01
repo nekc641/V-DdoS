726a60b2658d8e7d9054c0bf384b0dbfa96cd4b0
nekc641 <saekoyarezu09@gmail.com>
2024-10-01 20:55:32 +0800

    V-DdoS.py

--- V-DdoS.py
+++ V-DdoS.py
@@ -1,4 +1,4 @@
-print ("\033[91m")
+print ("\033[34m")
 import sys
 import os
 import time
@@ -19,16 +19,22 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet V-DdoS")
+os.system("figlet FEYNN")
+print("""
+ _____________________________________________
+|                  FEYNN DDOS                 |
+|           Version : miss na kita :(         |
+|_____________________________________________|
+""")
 print
-print "Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems"
+print "Ma priso nako gaw :c"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 os.system("clear")
 print("\033[93m")
 os.system("figlet DdoS Attack")
-print("Team : T34m V18rs")
+print("FEYNN FEYNN FEYNN FEYNNN")
 print ("\033[92m")
 print "[                    ] 0% "
 time.sleep(5)
