fe2e37389139fa6b4650d31ebe524ef16e790dd6
nekc641 <saekoyarezu09@gmail.com>
2024-10-01 20:40:33 +0800

    V-DdoS.py

--- V-DdoS.py
+++ V-DdoS.py
@@ -21,13 +21,6 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet V-DdoS")
 print
-print "Coded By : Mr.BL4Z3"
-print "Author   : T34m V18rs"
-print "Github   : github.com/T34mV18rs"
-print "Fb Page  : facebook.com/TeamVirusOfficial"
-print "FB Group : facebook.com/groups/mohinhossen"
-print "Telegram : t.me/Crackerspace"
-print "Join Cracker Space TG Group To Get Premium Apk(s) Free"
 print "Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems"
 print
 ip = raw_input("IP Target : ")
